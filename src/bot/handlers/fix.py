6f28a8d83d74bbeb60c31d8007075e3623a039d9
FrostX-Official <denialfrostiks589@gmail.com>
2025-01-14 19:22:46 +0300

    Update fix.py

--- src/bot/handlers/fix.py
+++ src/bot/handlers/fix.py
@@ -7,7 +7,7 @@ router = Router()
 @router.message(Command("fix"))
 async def fix_command(message: Message) -> None:
     """
-    Used to fix old keyboard inside bot ("Bot Commands" button)
+    Fixes old keyboard inside bot ("Bot Commands" button)
     """
 
     await message.answer("Fixing...", reply_markup=ReplyKeyboardRemove())
